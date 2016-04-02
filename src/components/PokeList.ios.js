96de0e3e6545037cfbf7cbf974f1834ae20a3566
Max <sync95@gmail.com>
2016-04-01 23:32:56 -0400

    Added PokeList Component with Fetch from Pokeapi

--- src/components/PokeList.ios.js
+++ src/components/PokeList.ios.js
@@ -3,18 +3,68 @@ import React, {
   Component,
   StyleSheet,
   Text,
+  ScrollView,
   View
 } from 'react-native';
+import {
+  Cell,
+  CustomCell,
+  Section,
+  TableView
+} from 'react-native-tableview-simple';
 
 export default class PokeList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {isFetching: true};
+    // this.setState({isFetching: true});
+  }
+  componentDidMount() {
+    fetch('http://pokeapi.co/api/v2/pokedex/2')
+    .then(r => r.json())
+    .then(r => {
+      console.log(r);
+      this.setState({isFetching: false, pokedex: r});
+    });
+  }
   render() {
+    var pokemonTable;
+    if(this.state.isFetching) {
+      pokemonTable = (
+        <View>
+          <Text style={styles.instructions}>
+            Loading!
+          </Text>
+        </View>
+      )
+    } else {
+      const { pokedex } = this.state;
+      var innerTable = pokedex.pokemon_entries.map(p => {
+        return (
+          <CustomCell
+            key={p.entry_number}
+            style={{width: 32, height: 32}}
+            onPress={() => {
+              console.log("You selected!". p.pokemon_species.name);
+            }}>
+            <Text style={{flex: 1, fontSize: 16}}>{p.entry_number}:  {p.pokemon_species.name}</Text>
+          </CustomCell>
+        )
+      });
+      pokemonTable = (
+        <TableView>
+          <Section header="Gen 1 <3">
+            {innerTable}
+          </Section>
+        </TableView>
+      );
+    }
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          PokeList!!!!
-        </Text>
-      </View>
-    );
+      <ScrollView contentContainerStyle={styles.stage}>
+        {pokemonTable}
+      </ScrollView>
+    )
+
   }
 }
 
@@ -35,4 +85,9 @@ const styles = StyleSheet.create({
     color: '#333333',
     marginBottom: 5,
   },
+  stage: {
+    backgroundColor: '#EFEFF4',
+    paddingTop: 50,
+    paddingBottom: 20
+  }
 });
